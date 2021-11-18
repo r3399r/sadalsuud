c3991952c4588f27e778383ff4352003c9a66187
yue.chang <r3399r@gmail.com>
2021-11-18 23:57:55 +0800

    improve type

--- backend/src/logic/VariablesService.ts
+++ backend/src/logic/VariablesService.ts
@@ -1,15 +1,18 @@
 import { injectable } from 'inversify';
+import { Variables } from 'src/model/variables';
 
 /**
  * Service class to return required parameters
  */
 @injectable()
 export class VariablesService {
-  public getParameters(name: string): { [key: string]: string } {
-    const nameArray: string[] = name.split(',');
-    const res: { [key: string]: string } = {};
+  public getParameters(name: string): Variables {
+    const nameArray: (keyof Variables)[] = name.split(
+      ','
+    ) as (keyof Variables)[];
+    const res: Variables = {};
     for (const val of nameArray) {
-      const envVariable = process.env[val];
+      const envVariable = process.env[val] as keyof Variables;
       if (envVariable !== undefined) res[val] = envVariable;
     }
 
---
+++ backend/src/model/variables.ts
@@ -0,0 +1,4 @@
+export type Variables = {
+  lineChannelUrl?: string;
+  lineLoginId?: string;
+};

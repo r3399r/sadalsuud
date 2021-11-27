8389d19ae65ed0e45e2b0baebe03e19b143bb94f
Yue Chang <r3399r@gmail.com>
2021-11-27 16:57:32 +0800

    improve unit test

--- frontend/src/app/services/login.service.spec.ts
+++ frontend/src/app/services/login.service.spec.ts
@@ -1,33 +1,37 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 import { LoginService } from './login.service';
 import { VariablesService } from './variables.service';
 
 describe('LoginService', () => {
   let service: LoginService;
   let variablesServiceSpy: jasmine.SpyObj<VariablesService>;
-  let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
     variablesServiceSpy = jasmine.createSpyObj('VariablesService', ['getVariables']);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
 
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [{ provide: VariablesService, useValue: variablesServiceSpy }],
+      providers: [
+        { provide: VariablesService, useValue: variablesServiceSpy },
+        { provide: HttpClient, useValue: httpClientSpy },
+      ],
     });
     service = TestBed.inject(LoginService);
-    httpClient = TestBed.inject(HttpClient);
-    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('checkLoginStatus should work', () => {
+  it('checkLoginStatus should return false if no localstorage is set', () => {
+    localStorage.removeItem('access_token');
     expect(service.checkLoginStatus()).toBeFalse();
+  });
+
+  it('checkLoginStatus should return true if localstorage is set', () => {
     localStorage.setItem('access_token', 'test');
     expect(service.checkLoginStatus()).toBeTrue();
     localStorage.removeItem('access_token');
@@ -36,5 +40,39 @@ describe('LoginService', () => {
   it('getLineLoginUrl should work', () => {
     const randomId = Math.random().toString();
     expect(service.getLineLoginUrl(randomId)).toContain(`client_id=${randomId}`);
+  });
+
+  it('login should work', async () => {
+    variablesServiceSpy.getVariables.and.resolveTo({
+      lineLoginId: 'test',
+      lineChannelKey: 'testKey',
+    });
+    httpClientSpy.post.and.returnValue(
+      of({ access_token: 'a', expires_in: 123, refresh_token: 'b' }),
+    );
+
+    sessionStorage.setItem('state', 'tempState');
+    await service.login({ state: 'tempState', code: 'abcd' });
+
+    expect(httpClientSpy.post).toHaveBeenCalledTimes(1);
+  });
+
+  it('login should fail', async () => {
+    variablesServiceSpy.getVariables.and.resolveTo({
+      lineLoginId: 'test',
+      lineChannelKey: 'testKey',
+    });
+    httpClientSpy.post.and.returnValue(
+      of({ access_token: 'a', expires_in: 123, refresh_token: 'b' }),
+    );
+
+    sessionStorage.setItem('state', 'tempState');
+
+    await expectAsync(service.login({ state: 'notSameState', code: 'abcd' })).toBeRejectedWithError(
+      'bad process',
+    );
+    expect(httpClientSpy.post).toHaveBeenCalledTimes(0);
+
+    sessionStorage.removeItem('state');
   });
 });
--- frontend/src/app/services/variables.service.ts
+++ frontend/src/app/services/variables.service.ts
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class VariablesService {
-  public variables: VariablesResponse;
+  private variables: VariablesResponse;
 
   constructor(private http: HttpClient) {
     this.variables = {};

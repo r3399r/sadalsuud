578cddf586ca444fa37381b9597f005d9dd038d3
yue.chang <r3399r@gmail.com>
2022-03-08 09:53:48 +0800

    improve ux in admin

--- frontend/src/app/pages/admin/admin.component.ts
+++ frontend/src/app/pages/admin/admin.component.ts
@@ -1,4 +1,8 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+import { ActivatedRoute, Params } from '@angular/router';
+import { PAGES } from 'src/app/constants/pages';
 
 @Component({
   selector: 'app-admin',
@@ -6,7 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent implements OnInit {
-  constructor() {}
+  selectedIndex: number | undefined;
 
-  ngOnInit(): void {}
+  constructor(private activatedRoute: ActivatedRoute, private location: Location) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((params: Params) => {
+      this.selectedIndex = params['tab'];
+    });
+  }
+
+  tabChanged(e: MatTabChangeEvent) {
+    this.location.replaceState(`${PAGES.ADMIN}?tab=${e.index}`);
+  }
 }

5ea67a64b138569f5732edbe51d3d248529fae2d
Yue Chang <r3399r@gmail.com>
2022-04-05 11:30:49 +0800

    fix shell script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -9,7 +9,7 @@ domain=celestialstudio.net
 echo ====================================================================================
 echo env: $env
 echo project: $project
-echo host: $host
+echo domain: $subDomain.$domain
 echo ====================================================================================
 
 echo deploy backend AWS...
@@ -17,7 +17,7 @@ cd ../backend2
 npm ci
 npm run pre:deploy
 aws cloudformation package --template-file aws/cloudformation/template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway
-aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env SubDomain=$subDomain Domain=$host --no-fail-on-empty-changeset --s3-bucket y-cf-midway
+aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env SubDomain=$subDomain Domain=$domain --no-fail-on-empty-changeset --s3-bucket y-cf-midway
 echo ====================================================================================
 
 echo deploy frontend to S3...

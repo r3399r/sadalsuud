48df491cbf94d3086164d8514f6f72be07b3731d
Yue Chang <r3399r@gmail.com>
2022-06-06 23:27:09 +0800

    modify sign form step

--- frontend/src/page/trips/component/SignForm.tsx
+++ frontend/src/page/trips/component/SignForm.tsx
@@ -39,72 +39,75 @@ const SignForm = ({ onClose, tripId = 'xxx' }: TripsFormProps) => {
 
   return (
     <form className={style.self} onSubmit={handleSubmit(onSubmit)}>
-      <div className={style.hint}>* 為必填項目</div>
       <FormRadio
         control={control}
         name="forWho"
-        defaultValue="self"
         items={[
           { value: 'self', label: '我自己要報名參加' },
           { value: 'kid', label: '幫小孩報名參加' },
         ]}
       />
-      <FormInput
-        control={control}
-        name="name"
-        rules={{ required: true }}
-        label={forWho === 'kid' ? '小孩的名字*' : '你的名字*'}
-        size="small"
-        error={errors.name !== undefined}
-      />
-      <FormInput
-        control={control}
-        name="phone"
-        rules={{ required: true, pattern: /^[0-9]+$/i }}
-        label={forWho === 'kid' ? '家長的聯絡電話*' : '聯絡電話*'}
-        helperText="此為通知時使用，通知中籤或通知活動內容"
-        size="small"
-        error={errors.phone !== undefined}
-      />
-      <FormInput
-        control={control}
-        name="line"
-        label={forWho === 'kid' ? '家長的 LINE ID' : 'LINE ID'}
-        helperText="若有 LINE ID，通知更方便"
-        size="small"
-      />
-      <FormInput
-        formType="yearPicker"
-        control={control}
-        name="yearOfBirth"
-        label={forWho === 'kid' ? '小孩的出生年(西元)*' : '出生年(西元)*'}
-        rules={{ required: true }}
-        helperText="此為遇到同名同姓時識別用"
-        size="small"
-        error={errors.yearOfBirth !== undefined}
-      />
-      {forWho === 'kid' && (
-        <div className={style.accompany}>
-          是否陪小孩同行:
-          <FormRadio
+      {forWho && (
+        <div className={style.main}>
+          <div className={style.hint}>* 為必填項目</div>
+          <FormInput
             control={control}
-            name="accompany"
-            defaultValue="yes"
-            items={[
-              { value: 'yes', label: '是' },
-              { value: 'no', label: '否' },
-            ]}
+            name="name"
+            rules={{ required: true }}
+            label={forWho === 'kid' ? '小孩的名字*' : '你的名字*'}
+            size="small"
+            error={errors.name !== undefined}
           />
+          <FormInput
+            control={control}
+            name="phone"
+            rules={{ required: true, pattern: /^[0-9]+$/i }}
+            label={forWho === 'kid' ? '家長的聯絡電話*' : '聯絡電話*'}
+            helperText="此為通知時使用，通知中籤或通知活動內容"
+            size="small"
+            error={errors.phone !== undefined}
+          />
+          <FormInput
+            control={control}
+            name="line"
+            label={forWho === 'kid' ? '家長的 LINE ID' : 'LINE ID'}
+            helperText="若有 LINE ID，通知更方便"
+            size="small"
+          />
+          <FormInput
+            formType="yearPicker"
+            control={control}
+            name="yearOfBirth"
+            label={forWho === 'kid' ? '小孩的出生年(西元)*' : '出生年(西元)*'}
+            rules={{ required: true }}
+            helperText="此為遇到同名同姓時識別用"
+            size="small"
+            error={errors.yearOfBirth !== undefined}
+          />
+          {forWho === 'kid' && (
+            <div className={style.accompany}>
+              是否陪小孩同行:
+              <FormRadio
+                control={control}
+                name="accompany"
+                defaultValue="yes"
+                items={[
+                  { value: 'yes', label: '是' },
+                  { value: 'no', label: '否' },
+                ]}
+              />
+            </div>
+          )}
+          <div className={style.buttons}>
+            <Button variant="outlined" color="error" type="button" onClick={onClose}>
+              取消
+            </Button>
+            <Button variant="contained" type="submit">
+              送出
+            </Button>
+          </div>
         </div>
       )}
-      <div className={style.buttons}>
-        <Button variant="outlined" color="error" type="button" onClick={onClose}>
-          取消
-        </Button>
-        <Button variant="contained" type="submit">
-          送出
-        </Button>
-      </div>
     </form>
   );
 };
